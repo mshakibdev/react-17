c253172dd6486a38d0c1130cf5da21d2288173c1
Md Shakib Hassan <shakib.hassan@niftycoders.com>
2023-03-08 20:38:34 +0600

    fetch data using custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from "./services/productService";
 import Spinner from "./Spinner";
-
+import useFetch from "./services/useFetch";
 export default function App() {
   const [size, setSize] = useState("");
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    /*  
-    using promise --- step 1
-    getProducts("shoes")
-      .then((response) => setProducts(response))
-      .catch((e) => setError(e))
-      .finally(() => setLoading(false));
-      */
-
-    /* using Promise with async/wait --- step -2 */
-    async function init() {
-      try {
-        const response = await getProducts("shoes");
-        setProducts(response);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    init();
-  }, []);
+  const {
+    data: products,
+    loading,
+    error,
+  } = useFetch("products?category=shoes");
 
   function renderProduct(p) {
     return (
--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,22 +1,21 @@
 import { useState, useEffect } from "react";
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
 export default function useFetch(url) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    /*  
-    using promise --- step 1
-    getProducts("shoes")
-      .then((response) => setProducts(response))
-      .catch((e) => setError(e))
-      .finally(() => setLoading(false));
-      */
-
-    /* using Promise with async/wait --- step -2 */
     async function init() {
       try {
-        const response = await getProducts("shoes");
-        setProducts(response);
+        const response = await fetch(baseUrl + url);
+        if (response.ok) {
+          const json = await response.json();
+          setData(json);
+        } else {
+          throw response;
+        }
       } catch (error) {
         setError(error);
       } finally {
@@ -24,6 +23,6 @@ export default function useFetch(url) {
       }
     }
     init();
-  }, []);
-
+  }, [url]);
+  return { data, error, loading };
 }
