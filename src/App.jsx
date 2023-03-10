0955b1914f61f9137ebefa5443a7cef98be83341
Md Shakib Hassan <shakib.hassan@niftycoders.com>
2023-03-10 19:56:15 +0600

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Spinner from "./Spinner";
-import useFetch from "./services/useFetch";
+import Products from "./Products";
+import { Route, Routes } from "react-router-dom";
+import Detail from "./Details";
+import Cart from "./Cart";
 export default function App() {
-  const [size, setSize] = useState("");
-
-  const {
-    data: products,
-    loading,
-    error,
-  } = useFetch("products?category=shoes");
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-
-  const filteredProducts = size
-    ? products.filter((product) =>
-        product.skus.find((sku) => sku.size === parseInt(size))
-      )
-    : products;
-  if (error) throw error;
-  if (loading) return <Spinner />;
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select
-              id="size"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            {size && <h2> Found {filteredProducts.length} items</h2>}
-          </section>
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </main>
       </div>
       <Footer />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
+
 ReactDOM.render(
   <ErrorBoundary>
-    <App />,
+    <BrowserRouter>
+      <App />,
+    </BrowserRouter>
   </ErrorBoundary>,
   document.getElementById("root")
 );
