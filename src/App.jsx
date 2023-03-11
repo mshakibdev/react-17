ac84282ad51dbee13f3a32ee5b81512e3374c5e1
Md Shakib Hassan <shakib.hassan@niftycoders.com>
2023-03-11 16:58:54 +0600

    lifting state and use function form to reference existing state

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ import Products from "./Products";
 import { Route, Routes } from "react-router-dom";
 import Detail from "./Details";
 import Cart from "./Cart";
+import { useState } from "react";
 export default function App() {
+  const [cart, setCart] = useState([]);
+  function addToCart(id, sku) {
+    setCart((items) => {
+      const itemInCart = items.find((item) => item.sku === sku);
+      /* 
+
+        itemInCart.quantity++
+        Don't do this.In react state is immutable
+      
+      */
+    });
+  }
   return (
     <>
       <div className="content">
